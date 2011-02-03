9c20675371c0a53c11675c1da9ee5f5200df4a61
Marc von Renteln <marc@von-renteln.de>
2011-02-03 17:50:58 +0100

    toString

--- grails-app/domain/dsa/held/Charakteristik.groovy
+++ grails-app/domain/dsa/held/Charakteristik.groovy
@@ -4,15 +4,17 @@ package dsa.held
 class Charakteristik {
 
 	String name
-	
+
 	String beschreibung
-	
-    static constraints = {
+
+	static constraints = {
 		name(unique:true, nullable:false, blank:false, maxLength:30)
 		beschreibung(nullable:true, maxLength:255)
-    }
-	
-	static mapping = {
-		tablePerHierarchy false
+	}
+
+	static mapping = { tablePerHierarchy false }
+
+	String toString() {
+		return name
 	}
 }
