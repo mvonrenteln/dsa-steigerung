c917d134ab3459f410e78baf0867f797acd8129b
Marc von Renteln <marc@von-renteln.de>
2011-02-25 16:34:15 +0100

    MySQL eingeführt

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -22,33 +22,42 @@
 
 dataSource {
 	pooled = true
-	driverClassName = "org.hsqldb.jdbcDriver"
-	username = "sa"
-	password = ""
 }
+
 hibernate {
 	cache.use_second_level_cache = true
 	cache.use_query_cache = true
 	cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
 }
+
 // environment specific settings
 environments {
 	development {
 		dataSource {
 			dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-			url = "jdbc:hsqldb:mem:devDB"
+			url = "jdbc:mysql://localhost/DSADB"
+			driverClassName = "com.mysql.jdbc.Driver"
+			username = "dsauser"
+			password = "45a2"
 		}
 	}
 	test {
 		dataSource {
 			dbCreate = "create-drop"
 			url = "jdbc:hsqldb:mem:testDb"
+			driverClassName = "org.hsqldb.jdbcDriver"
+			username = "sa"
+			password = ""
 		}
 	}
 	production {
 		dataSource {
 			dbCreate = "update"
 			url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+			url = "jdbc:mysql://localhost/DSADB"
+			driverClassName = "com.mysql.jdbc.Driver"
+			username = "dsauser"
+			password = "45a2"
 		}
 	}
 }
