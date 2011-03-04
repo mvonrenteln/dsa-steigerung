0e1f050b353329b3f009da16d99886828c1b5ccc
Marc von Renteln <marc@von-renteln.de>
2011-03-04 14:33:46 +0100

    test von findby

--- test/unit/dsa/held/TalentTests.groovy
+++ test/unit/dsa/held/TalentTests.groovy
@@ -29,7 +29,7 @@ class TalentTests extends GrailsUnitTestCase {
 		assert basisTalente?.size() == 3
 	}
 
-	Talent createTalent(String name, TalentGruppe talentGruppe, Talent.Typ typ, List eigenschaften) {
+	public static Talent createTalent(String name, TalentGruppe talentGruppe, Talent.Typ typ, List eigenschaften) {
 		def talent = new Talent(name: name, talentGruppe: talentGruppe, typ: typ, eigenschaften: eigenschaften, quelle: Quelle.OFFIZIELL)
 		return talent.save(failOnError: true);
 	}
